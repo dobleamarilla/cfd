7e1c8e4d28219dff4f552648f94edf774fb6b6a9
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2025-02-03 13:14:03 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Crear directorio de backups con permisos adecuados
-sudo mkdir -p /var/backups/tocdb
-sudo chown -R $USER:$USER /var/backups/tocdb
+sudo mkdir -p /var/backups/tocgamedb
+sudo chown -R $USER:$USER /var/backups/tocgamedb
 
 # Dar permisos para ejecutar zenity como root (si es necesario)
 echo 'Defaults:root !' | sudo tee -a /etc/sudoers
